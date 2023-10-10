55cde93d571ae88b78f747b36a08a5e39d050d29
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2023-10-10 09:52:40 +0300

    Serial algorithm test mode

--- HPC_lab1_serial/HPC_lab1_serial.cpp
+++ HPC_lab1_serial/HPC_lab1_serial.cpp
@@ -4,24 +4,11 @@
 
 #include "Matrix.h"
 
-int main(int argc, char* argv[])
+bool print_values = false;
+bool evaluation_test = false;
+
+void test_matrix_vector_multiplication(const size_t& size)
 {
-	bool print_values = false;
-
-	for (size_t i = 0; i < argc; i++)
-	{
-		if (strcmp(argv[i], "-p") == 0)
-		{
-			print_values = true;
-		}
-	}
-
-	size_t size;
-
-	std::cout << "Enter size of matrix and vector:";
-
-	std::cin >> size;
-
 	Matrix matrix(size);
 	Vector vector(size);
 	matrix.random_data_initialization();
@@ -46,5 +33,49 @@ int main(int argc, char* argv[])
 	}
 
 	std::cout << std::endl << "Time of execution: " << std::fixed << std::setprecision(40) << duration << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+	for (size_t i = 0; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-p") == 0)
+		{
+			print_values = true;
+		}
+		else if (strcmp(argv[i], "-t") == 0)
+		{
+			evaluation_test = true;
+		}
+	}
+
+	if (evaluation_test)
+	{
+		size_t evaluation_sizes[] = { 10, 100, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000 };
+
+		for (size_t i = 0; i < sizeof(evaluation_sizes) / sizeof(size_t); i++)
+		{
+			Matrix matrix(evaluation_sizes[i]);
+			Vector vector(evaluation_sizes[i]);
+			matrix.random_data_initialization();
+			vector.random_data_initialization();
+
+			std::cout << "Matrix size " << evaluation_sizes[i] << "x" 
+				<< evaluation_sizes[i] << ":" << std::endl;
+
+			test_matrix_vector_multiplication(evaluation_sizes[i]);
+		}
+		return 0;
+	}
+
+	size_t size;
+
+	std::cout << "Enter size of matrix and vector:";
+
+	std::cin >> size;
+
+	test_matrix_vector_multiplication(size);
+
+	return 0;
 
 }
